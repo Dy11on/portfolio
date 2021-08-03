2d990493022da3b72a25d480de25a3fcd444ebb6
Dy11on <26803229+Dy11on@users.noreply.github.com>
2021-08-02 23:37:10 -0400

    Changed to use environent variable for port, as needed for cloud run

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use actix_web::{Result,App,HttpRequest,web, HttpServer};
 use actix_files::*;
 use std::path::PathBuf;
-
+use std::env;
 
 async fn index(_req: HttpRequest) -> Result<actix_files::NamedFile> {
     let path: PathBuf = "./files/index.html".parse().unwrap();
@@ -17,13 +17,16 @@ async fn resume(_req: HttpRequest) -> Result<actix_files::NamedFile> {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let address = "0.0.0.0:";
+    let port: String = env::var("PORT").unwrap();
+    let ports = address.to_owned() + &port;
     HttpServer::new(|| 
                     App::new()
                     .route("/", web::get().to(index))
                     .route("/index.html", web::get().to(index))
                     .route("/resume", web::get().to(resume))
                     .service(Files::new("/static","./files").show_files_listing()))
-        .bind("0.0.0.0:8000")?
+        .bind(&ports)?
         .run()
         .await
 }
